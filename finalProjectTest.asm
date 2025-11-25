9ef434d12a055f2841c7efe9ba7059c650a91f88
RyanHH21 <152576892+RyanHH21@users.noreply.github.com>
2025-11-24 23:33:46 -0800

    Update finalProjectTest.asm

--- finalProjectTest.asm
+++ finalProjectTest.asm
@@ -1,7 +1,10 @@
+#Final Project: SNAKE GAME
+#Group Member: Camilo Hernandez, Ethan Caoile, Ryan Hoang
+
 .data
-# Framebuffer: 512 × 256 pixels, 4 bytes per pixel = 0x80000 bytes
+# Framebuffer: 512 × 256 pixels, 4 bytes per pixel = 0x80000 bytes (= 131072) 
 frameBuffer: .space 0x80000
-borderThickness: .word 4
+
 borderColor:     .word 0x0000FF    # blue
 snakeColor:      .word 0x00FF00    # green
 appleColor:      .word 0xFF0000    # red
@@ -11,29 +14,29 @@ backgroundColor: .word 0xFFFFFF    # white
 main:
     	#Clear screen
     	la  $t0, frameBuffer        # pointer to framebuffer
-    	li  $t1, 8192             # number of pixels (512 × 256)
-    	lw  $t2, backgroundColor    # white (0xFFFFFF)
+    	li  $t1, 2048               # number of tiles 64 * 32 = 2048 tiles
+    	lw  $t2, backgroundColor    # white color
     	
-clear_loop:
-    	sw  $t2, 0($t0)             # write pixel
-    	addi $t0, $t0, 4            # move to next pixel
-    	addi $t1, $t1, -1
-    	bnez $t1, clear_loop
+clearLoop:
+    	sw  $t2, 0($t0)			# store white color value to the frame
+    	addi $t0, $t0, 4		# move to next pixel
+    	addi $t1, $t1, -1		# number of tile decrese 1 each time loop call
+    	bnez $t1, clearLoop
     	
     	#Top wall
     	la  $t0, frameBuffer
-    	li  $t1, 64                # 64 pixels across
-    	lw  $t2, borderColor
+    	li  $t1, 64				#save spot for 64 tiles
+    	lw  $t2, borderColor	#load the border color to $t2
     	
 drawTopBorder:
-    	sw  $t2, 0($t0)
+    	sw  $t2, 0($t0)			#store the borderColor to the frameBuffer		
     	addi $t0, $t0, 4
     	addi $t1, $t1, -1
     	bnez $t1, drawTopBorder
     	
     	#Bottom wall
     	la  $t0, frameBuffer
-    	addi $t0, $t0, 7936      # bottom row start (255 * 2048)
+    	addi $t0, $t0, 7936      #start of bottom row: 31 x 64 x 4
     	li  $t1, 64
     	lw  $t2, borderColor
     	
@@ -43,20 +46,20 @@ drawBottomBorder:
     	addi $t1, $t1, -1
     	bnez $t1, drawBottomBorder
     	
-    	#left wall
-    	la $t0, frameBuffer
-    	li $t1, 256
+    	#Left wall
+    	la $t0, frameBuffer		#start at column 0
+    	li $t1, 32				#number of rows
     	lw $t2, borderColor
 drawBorderLeft:
     	sw $t2, 0($t0)
-    	addi $t0, $t0, 256
+    	addi $t0, $t0, 256		#add 256 to move to next row
     	addi $t1, $t1, -1
-    	bnez $t1,drawBorderLeft
+    	bnez $t1, drawBorderLeft
     	
-    	#right wall
-	la $t0, frameBuffer
-	addi $t0, $t0, 508        # start of last column
-	li $t1, 255                 # number of rows
+    	#Right wall
+	la $t0, frameBuffer	 	#start at column 63
+	addi $t0, $t0, 252	 	#rightmost column 
+	li $t1, 32		 		#number of rows
 	lw $t2, borderColor
 drawBorderRight:
     	sw $t2, 0($t0)
@@ -64,8 +67,8 @@ drawBorderRight:
     	addi $t1, $t1, -1
     	bnez $t1, drawBorderRight
 
-borders_done:
+exit:
     	# Exit program
    	li $v0, 10
-    	syscall
+    syscall
 	
