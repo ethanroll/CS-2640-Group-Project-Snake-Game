3b6da3e4eedb469fac65fba3c74b5e55872c2fdf
RyanHH21 <152576892+RyanHH21@users.noreply.github.com>
2025-12-06 09:18:57 -0800

    Update mainRunF.asm and snake array in .data

--- mainRunF.asm
+++ mainRunF.asm
@@ -7,11 +7,9 @@
 # Framebuffer: 512 × 256 pixels, 4 bytes per pixel = 0x80000 bytes (= 131072) 
 frameBuffer: .space 0x80000
 
-
 borderColor:     .word 0x0000FF    # blue
 appleColor:      .word 0xFF0000    # red
 backgroundColor: .word 0xFFFFFF    # white
-
 
 snakeRight:      .word 0x0000FF00  # FLAG=00
 snakeDown:       .word 0x0100FF00  # FLAG=01
@@ -22,32 +20,43 @@ snakeUp:         .word 0x0300FF00  # FLAG=03
 snakeHeadPosx:  .word 16          # $s4 will hold X
 snakeHeadPosy:  .word 13          # $s5 will hold Y
 
-# Add these new initializers for the tail
-snakeTailPosx:  .word 16      # $s2 will hold Tail X
-snakeTailPosy:  .word 13      # $s3 will hold Tail Y
+# Initial tail same as head
+snakeTailPosx:  .word 16          # $s2
+snakeTailPosy:  .word 13          # $s3
+
+# Snake body arrays
+snakeBodyX: .space 400   # max 100 segments
+snakeBodyY: .space 400
+growSnake: 	.word 0
+snakeLength:	.word 1
 
 .text
- .globl main
- 
+.globl main
 main:
-    # Initialize screen and apple
-    jal screenInitiation
-    jal spawnApple
+    	# Initialize screen and apple
+    	jal screenInitiation
+    	jal spawnApple
 
-    # Load initial position
-    lw $s4, snakeHeadPosx
-    lw $s5, snakeHeadPosy
-    
-    # load initial tail pos
-    lw $s2, snakeTailPosx
-    lw $s3, snakeTailPosy
-    
-    # draw head at start before loop start
-    lw $s7, snakeRight
-    jal drawHead
+    	# Load initial head position
+    	lw $s4, snakeHeadPosx
+    	lw $s5, snakeHeadPosy
 
-    # Start moving right
-    j moveRight
+    	# Load initial tail position
+    	lw $s2, snakeTailPosx
+    	lw $s3, snakeTailPosy
 
-        .include "movement.asm"
-        .include "finalProjectTest.asm"
+    	# --- Initialize snake body arrays with the head ---
+    	la $t0, snakeBodyX
+    	sw $s4, 0($t0)
+    	la $t0, snakeBodyY
+    	sw $s5, 0($t0)
+
+    	# Draw head at start
+    	lw $s7, snakeRight
+    	jal drawHead
+
+    	# Start moving right
+    	j moveRight
+
+		.include "movement.asm"
+		.include "finalProjectTest.asm"
